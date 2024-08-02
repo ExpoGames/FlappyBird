6c71fb1c6fdcea8f222f07894f9aa0cc7b1bd2f2
ExpoGames <82821697+ExpoGames@users.noreply.github.com>
2024-08-02 23:29:15 +0000

    f

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bot = new TelegramBot(TOKEN, {
 const port = process.env.PORT || 5000;
  const gameName = "connectDots";
 const queries = {};
-server.use(express.static(path.join(__dirname, 'connectDots')));
+server.use(express.static(path.join(__dirname, 'hyperCasualStore_Bot')));
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
 bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
 bot.on("callback_query", function (query) {
