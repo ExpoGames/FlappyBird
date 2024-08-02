b3d3223fea82c66a64b0d662c33833e5c357c0ae
ExpoGames <82821697+ExpoGames@users.noreply.github.com>
2024-08-02 23:14:49 +0000

    g

--- index.js
+++ index.js
@@ -7,23 +7,30 @@ const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 const port = process.env.PORT || 5000;
- const gameName = "connectDots";
+const gameName = "connectDots";
 const queries = {};
 server.use(express.static(path.join(__dirname, 'FlappyBird')));
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
 bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
 bot.on("callback_query", function (query) {
-    if (query.game_short_name !== gameName) {
-        bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
-    } else {
+    // if (query.game_short_name !== gameName) {
+    //     bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
+    // } else {
         queries[query.id] = query;
         let gameurl = "https://expogames.github.io/FlappyBird/";
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
         });
-    }
+   // }
 });
+
+bot.gameQuery((ctx) => {
+    let queryId = ctx.callbackQuery.id
+    let gameurl = "https://expogames.github.io/FlappyBird/"+queryId;
+    ctx.answerGameQuery(gameUrl)
+})
+
 bot.on("inline_query", function (iq) {
     bot.answerInlineQuery(iq.id, [{
         type: "game",
